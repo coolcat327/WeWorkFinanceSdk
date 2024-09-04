85bf735b7c97cbd879142a0c174dfe06d49613d5
liujihao <liujiahao@imobpay.com>
2024-09-04 15:35:35 +0800

    添加创建必要数据子目录的脚本和编译检查新增初始化检查以确认'data'目录下是否存在必要的子目录（voice、video、image、file、chat_data、call），若不存在则创建。此外，实现对'sdktools'文件的检查，若缺失，则从源代码编译。更新还包含文件存在性检查的逻辑调整，以及将移动文件操作从条件外移至确保不与存在检查冲突。

--- run.sh
+++ run.sh
@@ -1,7 +1,34 @@
 #!/bin/bash
+# 判断data文件夹下如果不存在voice文件夹，video文件夹，image文件夹，file文件夹，chat_data文件夹,call文件夹，则创建
+# 定义需要创建的子目录列表
+directories=("data/voice" "data/video" "data/image" "data/file" "data/chat_data" "data/call")
+
+# 创建顶层目录
+if [ ! -d "data" ]; then
+    mkdir data
+    if [ $? -ne 0 ]; then
+        echo "Failed to create directory 'data'"
+        exit 1
+    fi
+fi
+
+# 创建子目录
+for dir in "${directories[@]}"; do
+    if [ ! -d "$dir" ]; then
+        mkdir "$dir"
+        if [ $? -ne 0 ]; then
+            echo "Failed to create directory '$dir'"
+            exit 1
+        fi
+    fi
+done
 
 start_time=$(date +"%Y-%m-%d %T")
-
+# 判断如果不存在sdktools文件，则使用g++ C_sdk/sdktools.cpp -ldl -o sdktools编译
+if [ ! -f "sdktools" ]; then
+    echo "sdktools文件不存在，开始编译sdktools"
+    g++ C_sdk/tool_testSdk_new.cpp -ldl -o sdktools
+fi
 
 if [ -f "chat.jsonl" ] || [ -f "chatdata.jsonl" ]; then
     echo "chat.jsonl 或 chatdata.jsonl 文件已存在，请先备份或删除"
@@ -24,10 +51,7 @@ else
     echo "chat_list.xlsx 文件不存在，请先执行 chatMsg.py"
 fi
 
-if [ -f "chat.jsonl" ] || [ -f "chatdata.jsonl" ]; then
-    echo "开始移动本次拉取的记录到备份文件夹"
-    python3 move_file.py
-fi
+python3 mvFile.py
 
 end_time=$(date +"%Y-%m-%d %T")
 echo "任务开始执行时间：$start_time"
